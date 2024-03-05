ea4ba0a0e133b292ec83f1bfba2543376802f621
Daniel Moura Barros <danielbarros338@gmail.com>
2024-03-04 22:51:49 -0300

    testing environment

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
 import express from "./config/express";
 
+console.log('VARIAVEIS DE AMBIENTE',process.env);
+
 express.listen(process.env.PORT || 3030, () => console.log(`Server online on port ${process.env.PORT || 3030}`))
